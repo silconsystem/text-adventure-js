62062c817cef4c6bb8e010f21479df4eb31672cf
Rob <silcon.art.system@gmail.com>
2023-08-26 13:59:20 +0200

    Create gameStrings.js

---
+++ game-disks/gameStrings.js
@@ -0,0 +1,38 @@
+// string object storing different awnsers, description, etc...
+const gameStrings = () => ({
+  
+  battle: [
+    {
+      room: 'cellar',
+      name: 'joey',
+      playerResponse: [ 
+                        `You Swing At The Kangaroo !!!`,
+                        `joey will be no more !!!`,
+                        `you try to punch joey`,
+                        `take this, creepy kangaroo man !!!`,
+                        `right in the pouch !!!`,
+                      ],
+      enemyResponse: [
+                        `eat the pain !!!`,
+                        `chew on that !!!`,
+                        `aaaaaarrrgggh`,
+                        `wow, that stings !!!`,
+                        `you dirty ....!!!`,
+                     ],
+      modifierPositive: [
+                          `landed a lucky punch !!!`,
+                          'pulled a dirty trick',
+                          `found a weakspot`,
+                          `right on the nose`,
+                          `below the waist`,
+                        ],
+      modifierNegative: [
+                          `ouch, below the waist`,
+                          `that landed extra hard`,
+                          `you tripped`,                          
+                          `suckerpunched !!!`,
+                          `stepped on a bee...`,
+                        ],                
+    },
+  ],
+});
